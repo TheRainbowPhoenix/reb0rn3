3c146d5038ea65229dfaf33edc47414c2df3fce3
03 <03@purplr>
2023-08-09 19:11:46 +0200

    test base SSR site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel/serverless";
+import deno from "@astrojs/deno";
 
-import sitemap from '@astrojs/sitemap';
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://example.com",
+  integrations: [mdx(), sitemap(), svelte()],
+  output: "server",
+  adapter: vercel() // adapter: deno()
 });
